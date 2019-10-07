f1b323f3cfc8eadcbf7cb8fc6cf1e464b67efaeb
Maxim Shalashnikov <schalaschnikov@yandex.ru>
2019-10-07 13:26:33 +0300

    Initial Commit

---
+++ CarInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarInfo
+//
+//  Created by Максим Шалашников on 07.10.2019.
+//  Copyright © 2019 Maxim Shalashnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
